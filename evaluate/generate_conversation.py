bea3b977f8d4bb08b677fe0b5f5d94ef66322927
zaidalyafeai <alyafey22@gamil.com>
2025-04-04 12:56:20 +0300

    update generate conversation and upgrade judge

--- evaluate/generate_conversation.py
+++ evaluate/generate_conversation.py
@@ -10,7 +10,7 @@ async def process_dataset(
         language: str = "English",
         student_level: str = "Middle School",
         max_dataset_size: int = 100,
-        generator: LLMPredictor = None
+        predictor: LLMPredictor = None
     ):
         """Process a dataset and save the conversations"""
         # Load dataset
@@ -18,23 +18,27 @@ async def process_dataset(
         dataset = dataset.select(range(max_dataset_size))
 
         # Create hash for results directory
-        results_path = f"examples/{generator.student_model}/{generator.tutor_model}/{dataset_name}/{split}"
+        results_path = f"examples/{predictor.student_model}/{predictor.tutor_model}/{dataset_name}/{split}"
         os.makedirs(results_path, exist_ok=True)
-
-        # Process each problem
-        for i, problem in tqdm(enumerate(dataset)):
-            answer, conversation = await generator.predict_conversation(
-                problem["question"],
-                language,
-                student_level,
-            )
-            
-            config = {
-                "student_model": generator.student_model,
-                "tutor_model": generator.tutor_model,
+        config = {
+                "student_model": predictor.student_model,
+                "tutor_model": predictor.tutor_model,
                 "language": language,
                 "student_level": student_level,
             }
+        with open(f"{results_path}/config.json", "w") as f:
+            json.dump(config, f, indent=4)
+            
+        # Process each problem
+        for i, problem in tqdm(enumerate(dataset)):
+            answer, conversation = await predictor.predict_conversation(
+                problem["question"],
+                language,
+                student_level,
+                log = True
+            )
+            print("conversation length: ", len(conversation))
+            
             
             # Save conversation to file
             question_id = problem['id']
@@ -43,23 +47,22 @@ async def process_dataset(
                 "id": question_id,
                 "question": problem["question"],
                 "answer": problem["answer"],
+                "model_answer": answer,
                 "conversation": conversation,
             }
             
             with open(f"{results_path}/{question_id}.json", "w") as f:
                 json.dump(results, f, indent=4)
-            with open(f"{results_path}/config.json", "w") as f:
-                json.dump(config, f, indent=4)
 
 async def main():
-    student_model = "Qwen/Qwen2.5-3B-Instruct"
-    tutor_model = "Qwen/Qwen2.5-7B-Instruct"
-    generator = LLMPredictor(mode="tutor", student_model=student_model, tutor_model=tutor_model)
+    student_model = "Qwen/Qwen2.5-7B-Instruct"
+    tutor_model = "google/gemini-flash-1.5"
+    predictor = LLMPredictor(mode="tutor", student_model=student_model, tutor_model=tutor_model)
 
     await process_dataset(
         "gsm8k",
         "train",
-        generator = generator,
+        predictor = predictor,
         max_dataset_size=16
 
         ),
