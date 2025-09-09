8864ff17980282b0dd2076b9b6f94229563f48d8
zaidalyafeai <alyafey22@gamil.com>
2025-09-09 12:03:59 +0300

    script to load multiple models

--- src/serve.sh
+++ src/serve.sh
@@ -1,1 +1,12 @@
-CUDA_VISIBLE_DEVICES=0 vllm serve Qwen/Qwen2.5-7B-Instruct --dtype=half
+source /ibex/ai/home/alyafez/.vllm/bin/activate
+vllm serve \
+        /ibex/ai/home/alyafez/models/$1 \
+        --host localhost \
+        --port $3 \
+        --gpu-memory-utilization $2 \
+        --tensor-parallel-size 1 \
+        --pipeline-parallel-size 1 \
+        --max-model-len 2084 \
+        --max-num-seqs 50 \
+        --dtype=bfloat16 \
+	--served-model-name $1
---
+++ src/serve_multiple.sh
@@ -0,0 +1,2 @@
+bash src/serve.sh Qwen2.5-3B-Instruct 0.3 8787 &
+bash src/serve.sh Qwen2.5-7B-Instruct 0.65 8788
