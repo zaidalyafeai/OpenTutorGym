632d794a2e27c82082cca34edc09f68f916f62bf
zaidalyafeai <alyafey22@gamil.com>
2025-11-27 13:19:25 +0300

    update codes for starting the server

--- examples/start_tutor_server.sh
+++ examples/start_tutor_server.sh
@@ -1,10 +1,10 @@
 source /ibex/ai/home/alyafez/.vllm/bin/activate
-CUDA_VISIBLE_DEVICES=0,1 vllm serve \
+CUDA_VISIBLE_DEVICES=0 vllm serve \
         /ibex/ai/home/alyafez/models/$1 \
         --host 0.0.0.0 \
         --port $2 \
         --gpu-memory-utilization 0.95 \
-        --tensor-parallel-size 2 \
+        --tensor-parallel-size 1 \
         --pipeline-parallel-size 1 \
         --max-model-len 4096 \
         --max-num-seqs 50 \
