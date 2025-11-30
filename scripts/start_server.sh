3d33d8e42f0cfd54a3daebb521a044c59aa95815
zaidalyafeai <alyafey22@gamil.com>
2025-11-30 13:00:29 +0300

    move all the scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
-source /ibex/ai/home/alyafez/.vllm/bin/activate
-CUDA_VISIBLE_DEVICES=2,3 vllm serve \
-        /ibex/ai/home/alyafez/models/$1 \
+source .vllm/bin/activate
+CUDA_VISIBLE_DEVICES=0,1 vllm serve \
+        $1 \
         --host 0.0.0.0 \
         --port $2 \
         --gpu-memory-utilization 0.95 \
---
+++ scripts/start_student_server.sh
@@ -0,0 +1,12 @@
+source .vllm/bin/activate
+CUDA_VISIBLE_DEVICES=2,3 vllm serve \
+        $1 \
+        --host 0.0.0.0 \
+        --port $2 \
+        --gpu-memory-utilization 0.95 \
+        --tensor-parallel-size 2 \
+        --pipeline-parallel-size 1 \
+        --max-model-len 4096 \
+        --max-num-seqs 50 \
+        --dtype=bfloat16 \
+	--served-model-name $1
