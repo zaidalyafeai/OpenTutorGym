3441759078e2a548e6a8e7b481ad80e502dad2c8
zaidalyafeai <alyafey22@gamil.com>
2025-11-30 12:40:09 +0300

    remove old files

--- src/train.py
+++
@@ -1,6 +0,0 @@
-from Datasets import load_dataset
-from predict import TLLM
-
-dataset = load_dataset('comta')
-model = TLLM(model_name="Qwen/Qwen2.5-0.5B-Instruct")
-model.fine_tune(dataset, trainer_config_path="./configs/sft_config.yaml", peft_config_path="./configs/lora_config.yaml")
--- src/utils.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-import json
-
-
-def hash_question(question):
-    return hashlib.sha256(json.dumps(question).encode()).hexdigest()
